69d116bcaf461b9a7a29365e8514d64ccf06bcf0
jdamaster <julianx8.holgin@gmail.com>
2016-04-23 10:10:53 -0500

    hola

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 console.log("inicio del mini api");
 var http=require("http");
 var express=require("express");
-var app=expres();
+var app=express();
 var port=7894;
 app.listen(port,function(){
 	console.log("listen on port:" +port);
@@ -11,7 +11,7 @@ app.get("/",function(req,res){
 
 });
 var bodyParser=require("body-parser");
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.post("/postPrueba",function(requ,res){
 	console.log(req.body);
 	res.send("OK")
@@ -27,4 +27,32 @@ client.connect(function(err){
 	console.log("Conexion exitosa");
 	client.end();
 
+});
+app.post('/api/insertar', function(req, res){
+    var cedula = req.body.cedula;
+    var nombre = req.body.nombre;
+    var dinero = req.body.dinero;
+    
+    var queryInsertar = 'INSERT INTO saldo VALUES('
+                + cedula + ', '
+                + '\'' + nombre + '\', '
+                + dinero + ');'
+    console.log(queryInsertar);
+    
+    pg.connect(URL, function(err, client, done){
+        if (err){
+            res.send('Error :(');
+            return console.log('Error de conexión');
+        }
+        client.query(queryInsertar, function(err, result){
+            if(err){
+                res.send('Error :(');
+                client.end();
+                return console.log('Error en el query');
+            }
+            console.log('Se insertó');
+            res.send('OK c:');
+            client.end();
+        });
+    });
 });
